f53fdb4cda21c78cea4878137035a5daea65b747
Vicenzo Giordana <giordanavicenzo@gmail.com>
2026-06-09 11:34:20 -0300

    fix: listen on all interfaces for Docker

--- config/dev.exs
+++ config/dev.exs
@@ -24,9 +24,8 @@ config :alethea, Alethea.Repo,
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
 config :alethea, AletheaWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  # Binding to all interfaces for Docker
+  http: [ip: {0, 0, 0, 0}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
