0ce2b744268c72425cdded92ec3fbef2b6433dec
Vicenzo Giordana <giordanavicenzo@gmail.com>
2026-06-09 11:30:33 -0300

    fix: Docker dev setup - env vars for db, remove mix.lock from dockerignore, fix Dockerfile

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :alethea, Alethea.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "alethea_dev",
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  database: System.get_env("POSTGRES_DB", "alethea_dev"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
